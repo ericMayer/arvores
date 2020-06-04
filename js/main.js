2ba6c2c4b4570c0f2c14da462f4995de88c54997
Eric Mayer <eric.araujo_mayer@hotmail.com>
2020-06-04 16:21:46 -0300

    Criado movimentação básica do slide

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import Slide from "./modules/slide.js";
 
-const slide = new Slide('[data-slide="imagens"]');
+const slide = new Slide('[data-slide="imagens"]', '[data-slide="grid"]');
 
 slide.iniciar();
+
+// slide.moveSlide(4);
--- js/modules/slide.js
+++ js/modules/slide.js
@@ -1,6 +1,17 @@
 export default class Slide {
-  constructor(slide) {
+  constructor(slide, container) {
     this.slide = document.querySelector(slide);
+    this.container = document.querySelector(container);
+
+    // será armazenado a posição
+    // inicial do click
+    // o movimento realizado
+    // e a posição final
+    this.posicao = {
+      inicial: 0,
+      movimento: 0,
+      final: 0,
+    };
   }
 
   // quando for clicado com o mouse
@@ -9,31 +20,73 @@ export default class Slide {
     // para não ser possível arrastar a imagem do mouse
     event.preventDefault();
 
-    this.slide.addEventListener("mousemove", this.move);
+    // quando é realizado o primeiro click é armazenado
+    // a posição inicial
+    this.posicao.inicial = event.clientX;
+
+    // adicionando que está movendo o mouse
+    this.container.addEventListener("mousemove", this.move);
   }
 
   // método de quando está sendo movido o mouse após clicado
   move(event) {
-    // adicionado evento que verifica
-    // se foi parado o clique
-    this.slide.addEventListener("mouseup", this.up);
+    // chamado método que atualiza
+    // o movimento e armazenando
+    // em uma variável
+    const posicao = this.atualizaPosicao(event.clientX);
+
+    // chamando método que irá mover o slide
+    this.moveSlide(posicao);
   }
 
   // método de quando é parado o clique do mouse
   up() {
     // removendo o evento de mousemove,
     // pois já foi parado a movimentação do mouse
-    this.slide.removeEventListener("mousemove", this.move);
+    this.container.removeEventListener("mousemove", this.move);
 
-    // removendo evento de mouseup, pois
-    // só será usado quando foi feito a movimentação
-    this.slide.removeEventListener("mouseup", this.up);
+    // adiciando a posição final quando for finalizado
+    this.posicao.final = this.posicao.anterior;
+  }
+
+  // atualizando a posição do movimento
+  // em relação a posição inicial
+  // é retornado o movimento por final
+  atualizaPosicao(clientX) {
+    // subtraindo o movimento realizado
+    // da posição inicial, pois assim
+    // o valor fica negativo o slide
+    // se movimenta no sentido correto,
+    // caso fosse ao contrário o movimento do slide
+    // ficaria contrário ao natural
+    // multiplicando para ser mais rápido a movimentação
+    this.posicao.movimento = (this.posicao.inicial - clientX) * 1.4;
+
+    // pegando a posição final e diminuindo da posição do movimento
+    // para sempre que recomeçar o movimento,
+    // começar a partir da posição que foi
+    // foi finalizada o movimento
+    return this.posicao.final - this.posicao.movimento;
+  }
+
+  // método responsável
+  // por mover o slide
+  moveSlide(movimento) {
+    // movimentando o slide
+    this.slide.style.transform = `translate3d(${movimento}px, 0, 0)`;
+
+    // armazenando última posição
+    this.posicao.anterior = movimento;
   }
 
   // adiciona eventos iniciais
   addEvent() {
     // adicionado evento quando é clicado com o mouse
-    this.slide.addEventListener("mousedown", this.click);
+    this.container.addEventListener("mousedown", this.click);
+
+    // adicionado evento que verifica
+    // se foi parado o clique
+    this.container.addEventListener("mouseup", this.up);
   }
 
   // alterando referências dos eventos
