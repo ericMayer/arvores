d571938853ada6e1696edd85bf1b1fd5705f8829
Eric Mayer <eric.araujo_mayer@hotmail.com>
2020-06-15 23:06:07 -0300

    Adicionado slides passarem automáticos, e desabilitado botões próximo e anterior quando for o último e primeiro slide, também foi adicionado classe geral e adaptado slide, para caso estiver desabilitado javascript

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ import NavSlide from "./modules/navSlide.js";
 import Tooltip from "./modules/tooltip.js";
 import history from "./modules/history.js";
 
+document.documentElement.className += " js";
+
 const slide = new NavSlide(
   '[data-slide="anterior"]',
   '[data-slide="proximo"]',
